56960e2bc440d06a36f84fc58d6808c57cff1e97
kyitz <neb766@gmail.com>
2025-04-02 17:12:15 -0500

    Defining interface for InstrumentStrategy

--- src/edu/mu/myLittleMozart/strategy/AcousticGrandPianoStrategy.java
+++ src/edu/mu/myLittleMozart/strategy/AcousticGrandPianoStrategy.java
@@ -1,5 +1,13 @@
 package edu.mu.myLittleMozart.strategy;
 
 public class AcousticGrandPianoStrategy {
+	
+	public void applyInstrument(javax.sound.midi.Track track, int channel) throws javax.sound.midi.InvalidMidiDataException {
+		javax.sound.midi.ShortMessage message = new javax.sound.midi.ShortMessage();
+		message.setMessage(javax.sound.midi.ShortMessage.PROGRAM_CHANGE, channel, 0,0);
+		javax.sound.midi.MidiEvent event = new javax.sound.midi.MidiEvent(message, 0);
+		track.add(event);
+		
+	}
 
 }
--- src/edu/mu/myLittleMozart/strategy/InstrumentStrategy.java
+++ src/edu/mu/myLittleMozart/strategy/InstrumentStrategy.java
@@ -1,5 +1,6 @@
 package edu.mu.myLittleMozart.strategy;
 
 public interface InstrumentStrategy {
+	void applyInstrument(javax.sound.midi.Track track, int channel) throws javax.sound.midi.InvalidMidiDataException;
 
 }

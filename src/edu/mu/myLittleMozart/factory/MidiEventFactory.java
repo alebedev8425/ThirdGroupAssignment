7864af4ad7b51bfd5aab529bcd3554643b21ed2a
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-04-02 12:17:22 -0500

    Wrote code for interfaces for abstract factory and implemented the first concrete factory

--- src/edu/mu/myLittleMozart/factory/MidiEventFactory.java
+++ src/edu/mu/myLittleMozart/factory/MidiEventFactory.java
@@ -1,5 +1,33 @@
 package edu.mu.myLittleMozart.factory;
 
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.InvalidMidiDataException; 
+
 public interface MidiEventFactory {
+	
+	/**
+	 * 
+	 * @param tick
+	 * @param note
+	 * @param velocity
+	 * @param channel
+	 * @return
+	 * @throws InvalidMidiDataException
+	 */
+	
+	
+	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+	
+	/**
+	 * 
+	 * @param tick
+	 * @param note
+	 * @param channel
+	 * @return
+	 * @throws InvalidMidiDataException
+	 */
+	
+	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
+
 
 }

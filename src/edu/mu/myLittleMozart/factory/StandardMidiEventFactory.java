06d39791ff266df6ab40cdf62134802e1dc90487
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-04-02 12:26:06 -0500

    Finished coding the last two concrete factory classes

--- src/edu/mu/myLittleMozart/factory/StandardMidiEventFactory.java
+++ src/edu/mu/myLittleMozart/factory/StandardMidiEventFactory.java
@@ -1,5 +1,28 @@
 package edu.mu.myLittleMozart.factory;
 
-public class StandardMidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
+public class StandardMidiEventFactory implements MidiEventFactory{
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		ShortMessage noteOnShortMessage = new ShortMessage();
+		noteOnShortMessage.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		
+		
+		return new MidiEvent(noteOnShortMessage,tick);
+	}
+  
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		ShortMessage noteOffShortMessage = new ShortMessage();
+		noteOffShortMessage.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+		
+		return new MidiEvent(noteOffShortMessage, tick);
+	}
 
 }

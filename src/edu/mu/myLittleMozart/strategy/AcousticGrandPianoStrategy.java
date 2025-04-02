2f675870404b3c143eee15eb46b0d62c8a4b4370
kyitz <neb766@gmail.com>
2025-04-02 17:28:36 -0500

    adding AcousticGrandPiano, ElectricBassGuitar, and Trumpet strategies

--- src/edu/mu/myLittleMozart/strategy/AcousticGrandPianoStrategy.java
+++ src/edu/mu/myLittleMozart/strategy/AcousticGrandPianoStrategy.java
@@ -1,13 +1,14 @@
 package edu.mu.myLittleMozart.strategy;
+import javax.sound.midi.*;
 
-public class AcousticGrandPianoStrategy {
-	
-	public void applyInstrument(javax.sound.midi.Track track, int channel) throws javax.sound.midi.InvalidMidiDataException {
-		javax.sound.midi.ShortMessage message = new javax.sound.midi.ShortMessage();
-		message.setMessage(javax.sound.midi.ShortMessage.PROGRAM_CHANGE, channel, 0,0);
-		javax.sound.midi.MidiEvent event = new javax.sound.midi.MidiEvent(message, 0);
-		track.add(event);
-		
-	}
-
+public class AcousticGrandPianoStrategy implements InstrumentStrategy {
+    @Override
+    public void applyInstrument(Track track, int channel) throws InvalidMidiDataException {
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 0, 0);
+        MidiEvent event = new MidiEvent(message, 0);
+        track.add(event);
+    }
 }
+
+
--- src/edu/mu/myLittleMozart/strategy/TrumpetStrategy.java
+++ src/edu/mu/myLittleMozart/strategy/TrumpetStrategy.java
@@ -1,5 +1,13 @@
 package edu.mu.myLittleMozart.strategy;
 
-public class TrumpetStrategy {
+import javax.sound.midi.*;
 
+public class TrumpetStrategy implements InstrumentStrategy {
+    @Override
+    public void applyInstrument(Track track, int channel) throws InvalidMidiDataException {
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 56, 0); // Trumpet is 56
+        MidiEvent event = new MidiEvent(message, 0);
+        track.add(event);
+    }
 }
